b5165cdd1f326c2d577fed1bd82ecd2c34339515
ani_baka <ani_baka9@gmail.com>
2016-05-25 20:08:03 +0300

    del shit

--- tests/testmodul.cpp
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "testmodul.h"
-
-int testfunc(int a, int b)
-{
-	return a + b;
-}
--- tests/testmodul.h
+++
@@ -1,6 +0,0 @@
-#ifndef SYMBOL
-#define SYMBOL
-
-int testfunc(int a, int b);
-
-#endif
