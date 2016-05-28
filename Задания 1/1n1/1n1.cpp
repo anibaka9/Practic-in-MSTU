45bb7f9fcd7e4e75d2c942588854cd7c5baed471
ani_baka <ani_baka9@gmail.com>
2016-05-28 12:20:43 +0300

    Done 1n1

---
+++ Задания 1/1n1/1n1.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <iostream>
+#include "math.h"
+
+using namespace std;
+
+int resh(double a, double b, double c, double &x1, double &x2);
+
+int main(int argc, char const *argv[])
+{
+	double a, b, c;
+	int flag;
+	double x1, x2;
+	do
+	{
+		cout << "inp a, b, c ";
+		cin >> a >> b >> c;
+		if(a == 0) continue;
+		flag = resh(a, b, c, x1, x2);
+		if(flag == 0)
+			cout << "x1 = " << x1 << ", x2 = " << x2 << "\n";
+		else
+			cout << "no resh\n";
+	}
+	while(a != 0);
+	return 0;
+}
+
+
+int resh(double a, double b, double c, double &x1, double &x2)
+{
+	double dis = b * b - 4 * a * c;
+	if(dis >= 0)
+	{
+		x1 = (-b + sqrt(dis)) / (2 * a);
+		x2 = (-b - sqrt(dis)) / (2 * a);
+		return 0;
+	}
+	else
+		return 1;
+}
+
