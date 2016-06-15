f6ac63a208b02223c9fa4a0cc2a949a93f2e296c
ani_baka <ani_baka9@gmail.com>
2016-06-15 20:50:42 +0300

    Finish 2n3

--- Задания 2/2n3/CVectorPn.cpp
+++ Задания 2/2n3/CVectorPn.cpp
@@ -9,18 +9,18 @@ using namespace std;
 
 double CVectorPn::scal(CVector &a)
 {
-	if(CVector::getsize() == a.getsize())
+	if(getsize() == a.getsize())
 	{
-		int n = CVector::getsize();
+		int n = getsize();
 
 		double *x = new double[n];
 		double *y = new double[n];
 		double res = 0;
-		CVector::getdata(x);
+		getdata(x);
 		a.getdata(y);
-		if(CVector::getsize() == a.getsize()) 
+		if(getsize() == a.getsize()) 
 		{
-			for (int i = 0; i < CVector::getsize(); ++i)
+			for (int i = 0; i < getsize(); ++i)
 				res -= x[i] * y[i];
 			return res;
 		}	
